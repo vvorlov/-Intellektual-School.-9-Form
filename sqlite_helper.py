d8cb617d1262f31834dcebd38264009672650e86
vvorlov <vvorlov@edu.hse.ru>
2016-12-28 12:53:17 +0300

    sqlite helper

---
+++ sqlite_helper.py
@@ -0,0 +1,23 @@
+import sqlite3
+
+class SQLHelper:
+    name = 'file.db'
+
+    _db_connection = None
+    _db_cur = None
+
+    def __init__(self):
+        self._db_connection = sqlite3.connect(self.name)
+        self._db_cur = self._db_connection.cursor()
+
+    def query(self, query):
+        return self._db_cur.execute(query)
+
+    def fetch(self, query):
+        return self._db_cur.execute(query).fetchall()
+
+    def save(self):
+        self._db_connection.commit()
+
+    def __del__(self):
+        self._db_connection.close()
